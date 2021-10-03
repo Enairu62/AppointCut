b062cefd4687c9a74b701c8c3514cfd7a160ea4b
Raymondfg123 <chiburichi05@gmail.com>
2021-10-04 02:25:41 +0800

    Miguel

--- Web Server/app.js
+++ Web Server/app.js
@@ -1,26 +1,60 @@
-const http = require('http')
-const fs = require('fs')
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const {createPool} = require('mysql');
+const flash = require('express-flash');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(flash());
 
 
 
-const server = http.createServer(function(req,res){
-	res.writeHead(200, {'Content-Type':'text/html'})
-	fs.readFile('./ServerHTML/login.html',function(error,data){
-		if (error){
-			res.writeHead(404)
-			res.write('Error: File Not Found')
-		}else {
-			res.write(data)
-		}
-		res.end()
-	})
+// //MYSQL
+const db = require('database');
+
+const pool = createPool({
+    host: "127.0.0.1",
+    user: "root",
+    password: "BCCG",
+    database: "accountsdb"
+ })
+ 
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + "/login.html");
 })
 
-server.listen(port,function(error){
-	if(error){
-		console.log('Something went wrong', error)
-	} else {
-		console.log('Server is listening on port ' +port)
-	}
-})
+app.post('/form', function (req, res){
+    var email = req.body.email.toLowerCase();
+    var password = req.body.password.toLowerCase();
+    
+    var f = `Select SHA2("TITE",256)`
+    
+    // var sql = `INSERT INTO cocksizes (cockName, cockSize) VALUES ("${Email}", "${password}")`;
+  pool.query(`select Email , Password from users where Email = \"${email}\" AND Password = \"${password}\"` , (err, result) => {
+    if (err){
+      return console.log(err)
+    }
+      return console.log(result)
+    // console.log('Successfully logged in!');
+    // res.redirect('/try.html');
+  });
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.");
+});
+
+
+
+ 
+//  pool.query('select * from users', (err,result) =>{
+//     if(err){
+//         return console.log(err);
+//     }
+//         return console.log(result);
+// })
+
